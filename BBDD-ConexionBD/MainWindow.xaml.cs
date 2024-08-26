d409c599200f4ce36e2ebeb148447133799b575f
Waliva <walter.avendano@gmail.com>
2024-08-26 13:39:59 -0400

    Cargadando proyecto por primera vez

---
+++ BBDD-ConexionBD/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace BBDD_ConexionBD
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SqlConnection conexion = new SqlConnection("Data Source=localhost;Initial Catalog=tienda;Integrated Security=True;Encrypt=False");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConectar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                conexion.Open();
+                if (conexion.State == ConnectionState.Open)
+                {
+                    MessageBox.Show("La conexión ha sido exitosa");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally 
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
